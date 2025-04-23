d390aa5df70358189407e9299be25280bc00e6af
JihyunKim <kimwlgus001130@gmail.com>
2025-04-23 09:53:29 +0900

    initial commit

---
+++ src/main/java/com/kjh1130/dash_back/DashBackApplication.java
@@ -0,0 +1,13 @@
+package com.kjh1130.dash_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kjh1130/dash_back/DashBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjh1130.dash_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
